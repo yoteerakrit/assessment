c7a391ba0b7bd01d7ef67741a1134b673b6bfdd3
yo <O965236@tcrbank.com>
2022-12-26 13:57:05 +0700

    implement postgresql connection

--- repo.go/expense_repo_postgresql.go
+++ repo.go/expense_repo_postgresql.go
@@ -1,1 +1,29 @@
 package repo
+
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func NewPostgreSQL() *gorm.DB {
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable TimeZone=Asia/Bangkok",
+		"localhost",
+		"root",
+		"keep1234",
+		"keeplearning",
+		3306,
+	)
+
+	//"user=gorm password=gorm dbname=gorm port=9920 sslmode=disable TimeZone=Asia/Shanghai",
+	//"user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("failed to connect database : %s", err.Error())
+	}
+
+	return db
+}
